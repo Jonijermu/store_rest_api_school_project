9e02ae0da49590f300c5187a73fb66ee9a706f30
joni.heikkila <joni.heikkila@metropolia.fi>
2026-02-23 14:31:29 +0200

    Customer Entity id generation

--- src/main/java/com/store/entity/Customer.java
+++ src/main/java/com/store/entity/Customer.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "first_name", nullable = false)
