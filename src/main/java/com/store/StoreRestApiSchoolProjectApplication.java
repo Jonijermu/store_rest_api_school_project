0197a1199d64cf45ed91d14e5908510e7d6f45d2
joni.heikkila <joni.heikkila@metropolia.fi>
2026-02-23 13:19:25 +0200

    Initial commit

---
+++ src/main/java/com/store/StoreRestApiSchoolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreRestApiSchoolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreRestApiSchoolProjectApplication.class, args);
+	}
+
+}
